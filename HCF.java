97c140ba8a0ffd95350662feabc3ee8c29c21414
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-26 09:57:07 -0400

    Add files via upload

---
+++ HCF.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class HCF {
+    public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+     
+        int dividend, divisor;
+        int remainder, hcf = 0;
+        
+        System.out.print("Enter the first number ");
+        dividend = console.nextInt();
+        
+        System.out.print("Enter the second number ");
+        divisor = console.nextInt();        
+        
+        do
+	{
+            remainder = dividend % divisor;
+            
+            if(remainder == 0)
+            {
+                hcf = divisor;
+            }
+	    else
+            {
+                dividend = divisor;
+                divisor = remainder;
+	    }
+            
+        }while(remainder != 0);
+
+        System.out.println("HCF: " + hcf);
+    }  
+}
+
