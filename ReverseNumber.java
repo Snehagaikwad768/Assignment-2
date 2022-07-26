d7311116d72012285a00e77986ab5ed4151ea887
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-07-26 09:13:04 -0400

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.Scanner;
+public class ReverseNumber {
+     public static void main(String[] args)
+    {
+        Scanner console = new Scanner(System.in);
+     
+        int number;
+        int reverse = 0;
+        
+        System.out.print("Enter the number ");
+        number = console.nextInt();
+        
+        int temp = number;
+        int remainder = 0;
+        
+        while(temp>0)
+        {
+	    remainder = temp % 10;
+	    reverse = reverse * 10 + remainder;
+            temp /= 10;
+        }
+
+        System.out.println("Reverse of " + number + " is " + reverse);
+    }
+}
+
+
